003b44ed6a53487b8cf21447ba8fe070c4a61492
nicomarset <nicolasmarsetg@gmail.com>
2023-10-24 16:41:27 +0200

    segunda preentrega

---
+++ miproyecto/src/App.jsx
@@ -0,0 +1,42 @@
+/*import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+    <ItemListContainer title='Catálogo'/>
+    </>
+  )
+}
+
+export default App
+*/
+
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import Footer from './components/Footer/Footer'
+import Home from './components/Inicio/Home'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <NavBar/>
+
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path="/products" element={<ItemListContainer />} />
+        <Route exact path='/category/:categoryName' element={<ItemListContainer />} />
+        <Route exact path='/products/:productId' element={<ItemDetailContainer />} />
+      </Routes>
+
+    <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App
